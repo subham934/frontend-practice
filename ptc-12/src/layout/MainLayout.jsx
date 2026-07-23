b0274ae54a218d9e7db997f6783b38d0afe6fa80
Subham Dhar <siddy934@gmail.com>
2026-07-23 09:14:33 +0530

    axios

--- ptc-12/src/layout/MainLayout.jsx
+++ ptc-12/src/layout/MainLayout.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import { Outlet } from 'react-router'
-import Navbar from '../components/Navbar' 
+import React from "react";
+import { Outlet } from "react-router";
+import Navbar from "../components/Navbar";
 const MainLayout = () => {
   return (
-    <div>
-        <Navbar/>
-        <Outlet/>
+    <div className="w-full min-h-screen bg-black text-white">
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
---
+++ ptc-13/src/App.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+const App = () => {
+  const [data, setData] = useState([]);
+  const [Page, setPage] = useState(1);
+  const getData = async () => {
+    const res = await axios.get("https://picsum.photos/v2/list", {
+      params: {
+        page: Page,
+        limit: 10,
+      },
+    });
+    console.log(res.data);
+    setData(res.data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, [Page]);
+
+  return (
+    <div className="p-5">
+      <button
+        // onClick={() => getData()}
+        className="py-1 px-8 rounded-md bg-blue-500 cursor-pointer active:scale-95"
+      >
+        Get Data
+      </button>
+      <div className="mt-5 flex flex-wrap gap-5 justify-center">
+        {data &&
+          data.map((item) => (
+            <div
+              className="w-60 h-60 bg-zinc-300 rounded-md flex items-center justify-center overflow-hidden"
+              key={item.id}
+            >
+              <img
+                className="w-full h-full object-cover "
+                src={item.download_url}
+                alt=""
+              />
+            </div>
+          ))}
+      </div>
+
+      <div className="flex justify-center gap-3 mt-5">
+        <button
+          onClick={() => Page > 1 && setPage(Page - 1)}
+          className="bg-blue-500  px-5 font-black text-white py-1 rounded-xl"
+        >
+          Prev
+        </button>
+        <button
+          onClick={() => setPage(Page + 1)}
+          className="bg-green-500  px-5 font-black text-white py-1 rounded-xl"
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
